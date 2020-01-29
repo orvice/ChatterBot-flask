06ece0dd6290d986b4841931b3e81f6259419d5a
orvice <orvice@gmail.com>
2020-01-29 17:08:09 +0800

    update

--- bot.py
+++ bot.py
@@ -4,13 +4,6 @@ from chatterbot.trainers import ListTrainer
 
 chatbot = ChatBot("deepThought",
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
-    logic_adapters=[
-        "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.TimeLogicAdapter",
-        "chatterbot.logic.BestMatch"
-    ],
-    input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter",
     database="/data/database.db")
 
 trainer = ChatterBotCorpusTrainer(chatbot)
