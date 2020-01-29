0c06d02ca3ea994c8959c0737d59359ec46fff3e
orvice <orvice@gmail.com>
2020-01-29 15:40:56 +0800

    update bot

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-chatbot = ChatBot("deepThought")
+chatbot = ChatBot("deepThought",storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
+    database="/data/database.json")
 chatbot.set_trainer(ChatterBotCorpusTrainer)
 chatbot.train("chatterbot.corpus.chinese")
 
@@ -10,5 +12,6 @@ def talk(msg):
     return chatbot.get_response(msg)
 
 def train(input,res):
+    chatbot.set_trainer(ListTrainer)
     chatbot.train([input,res])
     return "ok"
